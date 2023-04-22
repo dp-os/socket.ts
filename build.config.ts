67868a7a0e09efae7bf9bb3c5ac175c47437fe4e
lzxb <lzxb@lzxbdeMacBook-Pro.local>
2023-04-22 10:59:00 +0800

    feat: init

---
+++ build.config.ts
@@ -0,0 +1,18 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  clean: true,
+  entries: [
+    {
+      input: './src/',
+      format: 'esm',
+      ext: 'mjs',
+      declaration: true
+    },
+    {
+      input: './src/',
+      format: 'cjs',
+      ext: 'js'
+    }
+  ]
+})
